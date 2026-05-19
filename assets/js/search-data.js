c51b226607bf4bae1b8094c322642adc74cea46a
shinben0327 <shinben0327@gmail.com>
2026-05-19 21:30:18 +0000

    Deploying to gh-pages from @ shinben0327/shinben0327.github.io@b61d9ac1b78a6c1f26074f0317f2f6d0a73639b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -36,16 +36,26 @@ ninja.data = [{
           section: "News",},{id: "news-i-have-started-my-msc-in-robotics-systems-and-control-at-eth-zurich",
           title: 'I have started my MSc in Robotics, Systems and Control at ETH Zurich....',
           description: "",
-          section: "News",},{id: "projects-human-object-interaction-retargeting-ongoing",
-          title: 'Human-Object Interaction Retargeting (ongoing)',
+          section: "News",},{id: "projects-human-object-interaction-retargeting",
+          title: 'Human-Object Interaction Retargeting',
           description: "Retargeting human-object interactions to robots using constrained optimisation and reinforcement learning.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/hoi_retarget/";
-            },},{id: "projects-nomadz-robocup",
-          title: 'NomadZ RoboCup',
+            },},{id: "projects-nomadz-humanoid-soccer-team",
+          title: 'NomadZ Humanoid Soccer Team',
           description: "Teaching robots to play soccer using motion imitation reinforcement learning to participate in the RoboCup competition.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/nomadz/";
+            },},{id: "projects-control-interface-for-tendon-driven-hand",
+          title: 'Control Interface for Tendon-driven Hand',
+          description: "A control interface for managing the movements of a tendon-driven robotic hand.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ori_hand/";
+            },},{id: "projects-qronk-3d-printed-quadruped-robot",
+          title: 'Qronk 3D-printed Quadruped Robot',
+          description: "Creating a 3D-printed quadruped robot as a student society.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/qronk/";
             },},{
         id: 'social-github',
         title: 'GitHub',
